b455eeaef974fdde17336acd72ec3881140337c4
garrett <g@rre.tt>
2010-07-22 13:02:43 -0500

    Updated references in dos.

--- doc/conf.py
+++ doc/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Landshark'
-copyright = u'2009, Garrett Smith'
+copyright = u'2010, Garrett Smith <g@rre.tt>'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
